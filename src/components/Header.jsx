834dac43f724e610fd34e721e8490e75dabee173
rahulmandyal1 <rahulmandyal079@gmail.com>
2024-06-17 22:28:47 +0530

    Home UI  WIP 🎉

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="md:text-7xl text-4xl font-bold text-red-500 mb-2 italic ">
+        CODE <span className="text-white">ROAST</span>
+      </h1>
+      <p className="text-gray-400 mb-12 md:text-2xl text-md mt-1 text-center">
+        get a free design audit powered by AI
+      </p>
+    </>
+  );
+};
+
+export default Header;
